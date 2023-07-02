7e0cb5ec6fdc6af5206d9de21b13065a7297569e
tejaswinikushal <tejavangimalla@gmail.com>
2023-07-02 08:58:51 +0530

    first commit

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/welcome")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return """Company Name: ABC Corporation\n
+Location: India\n
+Contact Detail: 999-999-9999"""
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
